0fc76c1d9ea3cd90e7e44bea12a76a2913baf44a
Humberto Flores <hflores@nhaschools.com>
2019-04-22 15:40:31 -0400

    Final Product

---
+++ reddit.service.js
@@ -0,0 +1,12 @@
+    
+function RedditService (http) {
+    const service = this;
+
+    service.fetchAwwSubreddit = () => {
+        
+    return http.get("https://www.reddit.com/r/aww/.json");
+    }
+}
+
+angular.module('RedditServiceApp')
+.service('RedditService', ['$http', RedditService])
